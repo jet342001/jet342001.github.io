c9ee1a47fc7033fce17549c088a7f11e3b20564d
jet342001 <jet342001@yahoo.com>
2020-05-09 17:59:46 -0600

    again

--- Project 1/js/myJS.js
+++ Project 1/js/myJS.js
@@ -1,2 +1,12 @@
-const options = [weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'];
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options)
+let date = new Date();
+
+function writedate() {
+    // MM/DD/YYYY HH:MM:SS
+    let hour = date.getHours();
+    var hourText = hour;
+    if (hour > 12) {
+        hourText = hour - 12;
+        timeOfDay = 'PM';
+    }
+    document.getElementById("lastUpdate").innerHTML = document.lastModified;
+}
