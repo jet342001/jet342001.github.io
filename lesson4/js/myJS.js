b24f2dd1ae11fb95a6efa2b622460bbcd5c79774
jet342001 <jet342001@yahoo.com>
2020-05-16 17:30:52 -0600

    Update JS to show current date in correct format

--- lesson4/js/myJS.js
+++ lesson4/js/myJS.js
@@ -1,4 +1,3 @@
-let date = new Date();
 document.addEventListener('DOMContentLoaded', onDocumentLoad); 
 
 const hambutton = document.querySelector('.ham');
@@ -9,13 +8,39 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 function onDocumentLoad(){
     writedate();
 }
+let date = new Date();
+
 function writedate() {
     // MM/DD/YYYY HH:MM:SS
-    let hour = date.getHours();
-    var hourText = hour;
-    if (hour > 12) {
-        hourText = hour - 12;
-        timeOfDay = 'PM';
-    }
-    document.getElementById("lastUpdate").innerHTML = document.lastModified;
+
+    var n = date.getDay();
+    var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+    var myDay = weekday[n];
+    var month = new Array(12);
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
+    var m = date.getMonth();
+    var myMon = month[m];
+    var dayNum = date.getDate()
+    var myYear = date.getFullYear();
+    var tday = myDay + ", "  + dayNum +" " + myMon + " " + myYear;
+
+    document.getElementById("lastUpdate").innerHTML = tday;
 }
