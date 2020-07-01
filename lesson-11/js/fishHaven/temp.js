ffcea3f87f53eff186c6eff9181772ca8bda61d0
jet342001 <jet342001@yahoo.com>
2020-07-01 09:19:21 -0600

    Adds update for Fish Haven

---
+++ lesson-11/js/fishHaven/temp.js
@@ -0,0 +1,14 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=83287,us&units=imperial&APPID=6c8279d80c5abede95f577b29a3bdcb0';
+fetch(url)
+.then((response) => response.json())
+.then((preston) => {
+  console.log(preston);
+  
+document.getElementById('current').textContent = Math.round(preston.main.temp);
+document.getElementById('high').textContent = Math.round(preston.main.temp_max);
+document.getElementById('hum').textContent = preston.main.humidity;
+document.getElementById('speed').textContent = Math.round(preston.wind.speed) + "MPH";
+document.getElementById('condition').textContent = preston.weather[0].main;
+document.getElementById('feelsLike').textContent = Math.round(preston.main.feels_like);
+
+});
